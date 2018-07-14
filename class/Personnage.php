677e88c49529b2b793f8565f391bac5c1994d0b1
Eric S <eric.stoclet@ibm.com>
2018-07-15 00:07:56 +0200

    Ajout class Personnage

---
+++ class/Personnage.php
@@ -0,0 +1,17 @@
+<?php
+class Personnage {
+    private $_force = 50;
+    private $_localisation = 'Lyon';
+    private $_experience = 1;
+    private $_degats = 0;
+
+    public function deplacer() {
+
+    }
+    public function frapper() {
+
+    }
+    public function gagnerExperience() {
+        
+    }
+}
